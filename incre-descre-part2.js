3acd41f98010a15987ed4286cb5a3a19f37ff941
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-08 18:05:19 -0300

    Add files via upload

---
+++ incre-descre-part2.js
@@ -0,0 +1,37 @@
+/*Criar uma variável com um Number fazer uma incrementação 5 
+  Criar outra variável com um Number fazer uma incrementação 5 de outra forma*/
+
+  //Incrementando com um valorsetado com mais de uma maneira
+  var a = 10;
+  a = a +5;
+  console.log(a);
+
+  var b = 20;
+  b += 5;
+  console.log(b);
+
+  var c = 30;
+  c++;
+  c++;
+  c++;
+  c++;
+  c++;
+  console.log(c);
+
+  //Decrementando com um valor setado com mais de uma maneira
+
+  var d = 20;
+  d = d -5;
+  console.log(d);
+
+  var e = 50;
+  e -= 5;
+  console.log(e);
+
+  var f = 70;
+  f--;
+  f--;
+  f--;
+  f--;
+  f--;
+  console.log(f)
